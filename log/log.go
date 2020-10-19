932c1e4dd9f88ed67aaf23cc9a4fe99bd61fd4ff
Krishna Iyer Easwaran <mail@krishnaiyer.dev>
2020-10-19 21:02:01 +0200

    Add better abstraction layer

--- log/log.go
+++ log/log.go
@@ -12,51 +12,139 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package log provides commonly used logging functions by wrapping the uber-go/zap package.
 package log
 
 import (
 	"context"
+	"reflect"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-type loggerKeyType string
+var reflectTypeToZapFieldType = map[reflect.Kind]zapcore.FieldType{
+	reflect.String: zapcore.StringType,
+	reflect.Int64:  zapcore.Int64Type,
+}
 
-// Field is a zapcore.Field
-type Field zapcore.Field
+type loggerKeyType string
 
 var loggerKey loggerKeyType = "logger"
 
-// FromContext retrieves a logger from a context and panics if there isn't one.
-func FromContext(ctx context.Context) *zap.Logger {
+// Logger wraps zap.Logger.
+type Logger struct {
+	ctx    context.Context
+	logger *zap.Logger
+	fields []zap.Field
+	err    error
+}
+
+// Options is the logger options.
+type Options struct {
+}
+
+// Field represents a logger field.
+type Field struct {
+	Key   string
+	Value interface{}
+}
+
+// New creates a new logger. Make sure to call defer logger.Clean() after calling this.
+// TODO: Benchmarking
+func New(ctx context.Context, debug bool) (*Logger, error) {
+	config := zap.NewProductionConfig()
+	config.Encoding = "console"
+	if !debug {
+		config.DisableStacktrace = true
+		config.EncoderConfig.CallerKey = ""
+		config.EncoderConfig.TimeKey = ""
+		config.DisableCaller = true
+	}
+	logger, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+	return &Logger{
+		ctx:    ctx,
+		logger: logger,
+		fields: make([]zap.Field, 0),
+	}, nil
+}
+
+// Clean cleans up the log states. Make sure to call this after creating a new logger.
+func (l *Logger) Clean() {
+	l.logger.Sync()
+}
+
+// NewContextWithLogger returns a new context with a logger and panics if it doesn't match the interface.
+func NewContextWithLogger(parentCtx context.Context, logger *Logger) context.Context {
+	if logger == nil {
+		panic("Nil Logger")
+	}
+	return context.WithValue(parentCtx, loggerKey, logger)
+}
+
+// NewLoggerFromContext retrieves a logger from a context and panics if there isn't one.
+func NewLoggerFromContext(ctx context.Context) *Logger {
 	val := ctx.Value(loggerKey)
-	logger, ok := val.(*zap.Logger)
+	logger, ok := val.(*Logger)
 	if !ok {
 		panic("No logger in context")
 	}
 	return logger
 }
 
-// New returns a new zap logger.
-func New() (*zap.Logger, error) {
-	cfg := zap.Config{
-		DisableCaller:     true,
-		DisableStacktrace: true,
-		Encoding:          "console",
-	}
-	logger, err := cfg.Build()
-	if err != nil {
-		return nil, err
-	}
-	defer logger.Sync()
-	return logger, nil
+// Debug logs a Debug level message.
+func (l *Logger) Debug(msg string) {
+	l.logger.With(l.fields...).Debug(msg)
 }
 
-// NewContext returns a new context with a logger and panics if a nil value is passed.
-func NewContext(ctx context.Context, logger *zap.Logger) context.Context {
-	if logger == nil {
-		panic("Nil Logger")
+// Info logs a Info level message.
+func (l *Logger) Info(msg string) {
+	l.logger.With(l.fields...).Info(msg)
+}
+
+// Warn logs a Warning level message.
+func (l *Logger) Warn(msg string) {
+	l.logger.With(l.fields...).Warn(msg)
+}
+
+// Error logs a Error level message.
+func (l *Logger) Error(msg string) {
+	l.logger.With(l.fields...).Error(msg)
+}
+
+// Fatal logs a Fatal message.
+func (l *Logger) Fatal(msg string) {
+	l.logger.With(l.fields...).Fatal(msg)
+}
+
+// WithField returns a logger with the provided field.
+func (l *Logger) WithField(key string, val interface{}) *Logger {
+	fieldType := reflectTypeToZapFieldType[reflect.TypeOf(val).Kind()]
+	switch fieldType {
+	case zapcore.StringType:
+		l.fields = append(l.fields, zap.Field{Key: key, String: val.(string), Type: fieldType})
+	case zapcore.Int64Type:
+		l.fields = append(l.fields, zap.Field{Key: key, Integer: val.(int64), Type: fieldType})
+	default:
+		// Skip this since we don't know the type
 	}
-	return context.WithValue(ctx, loggerKey, logger)
+	return l
+}
+
+// WithFields returns a logger with the providedfields.
+func (l *Logger) WithFields(fields []Field) *Logger {
+	for _, field := range fields {
+		l.fields = append(l.fields, zap.Field{Key: field.Key, Interface: field.Value})
+	}
+	return l
+}
+
+// WithError returns a logger with the current error padded.
+// TODO: How to append this error?
+func (l *Logger) WithError(err error) *Logger {
+	l.err = err
+	return l
 }
